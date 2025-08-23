a811bf95ec7974728e86054ad05d6eafaa0c5ee4
Shasank Prasad <shasankp.14@gmail.com>
2025-08-23 18:33:03 +0530

    Add passthrough copy for user media images

--- .eleventy.js
+++ .eleventy.js
@@ -539,6 +539,7 @@ module.exports = function (eleventyConfig) {
 
 
   eleventyConfig.addPassthroughCopy("src/site/img");
+  eleventyConfig.addPassthroughCopy("src/site/img/user/media");
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
   eleventyConfig.addPlugin(faviconsPlugin, { outputDir: "dist" });
